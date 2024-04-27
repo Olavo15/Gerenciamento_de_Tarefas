d21f03c2ede210bd453ad4a1dadb57c3a3b76bb0
aristotelesAlves <arystotelys@gmail.com>
2024-04-27 16:20:21 -0300

    rotas concluida!

--- my-project/src/router.tsx
+++ my-project/src/router.tsx
@@ -3,25 +3,52 @@ import Home from './pages/Home'
 import Layout from './pages/Layout/Layout'
 import PagNotFund from './pages/PagNotFund'
 
+
 export const Router = createBrowserRouter([
+    {
+        path:'/login',
+        element: <div>login</div>
+    },
+    {
+        path:'/cadastro',
+        element: <div>Cadastro</div>
+    },
     {
         path: '/',
         element: <Layout/>,
         errorElement:<PagNotFund/>,
         children:[
             {
-                path:'/',
+                path:'',
                 element: <Home/>
             },
             {
-                path:'/equipes',
+                path:'equipes',
                 element: <div>equipes</div>
             },
             {
-                path:'/projetos',
+                path:'projetos',
                 element: <div>projetos</div>
             },
         ]
     },
 
+    {
+        path:'/projeto/:idprojeto',
+        element:<div>Projeto do id</div>,
+        children:[
+            {
+                path:'',
+                element:<div>Home da tela do projeto!</div>
+            },
+            {
+                path:'equipe',
+                element:<div>listagem de mebros da equipe</div>
+            },
+            {
+                path:'calendario',
+                element:<div>Listagem de tarefas pendente com data</div>
+            },
+        ]
+    }
 ])
