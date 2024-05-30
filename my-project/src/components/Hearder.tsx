ca9078086e2a55c5c0fbe855ad342878439b261b
aristotelesAlves <arystotelys@gmail.com>
2024-05-30 01:55:37 -0300

    migrations + função integrada com front para criar projeto!

--- my-project/src/components/Hearder.tsx
+++ my-project/src/components/Hearder.tsx
@@ -3,9 +3,8 @@ import { useState } from "react";
 
 const Hearder = () => {
   const localStore = localStorage.getItem('userData')
-  // const {token} = JSON.parse(localStore ? localStore : '')
-  // const user: string = token.nome
-  const [perfil, setPerfil] = useState(false)
+  const {token} = JSON.parse(localStore ? localStore : '')
+  const user: string = token.nome
   return (
     <header className="h-fit flex flex-col">
       <div className="w-full h-14 bg-zinc-300 flex items-center justify-between px-4 py-2">
@@ -27,7 +26,7 @@ const Hearder = () => {
         
         <div className="flex items-center gap-1 relative">
           <p>
-            {/* {user} */}
+            {user}
           </p>
           <div className="flex gap-2 items-center">
               <button onClick={() => {localStorage.removeItem('userData'), window.location.href = '/login'}}> <SignOut size={20}/></button>
--- my-project/src/router.tsx
+++ my-project/src/router.tsx
@@ -3,7 +3,6 @@ import Layout from './pages/Layout/Layout'
 import PagNotFund from './pages/PagNotFund'
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
-import Recuperacao from './pages/Recuperacao'
 import Projetos from './pages/projetos'
 import { Projeto } from './pages/Projeto'
 
@@ -18,15 +17,11 @@ console.log(user)
 export const Router = createBrowserRouter([
     {
         path:'/login',
-        element: <Login/>
+        element: user ?  <Navigate to="/" replace /> : <Login/> 
     },
     {
         path:'/cadastro',
-        element: <Cadastro/>
-    },
-    {
-        path:'/recuperacao',
-        element: <Recuperacao/>
+        element: user ?  <Navigate to="/" replace /> : <Cadastro/> 
     },
     {
         path: '/',
@@ -43,23 +38,4 @@ export const Router = createBrowserRouter([
             },
         ]
     },
-
-    // {
-    //     path:'/projeto/:idprojeto',
-    //     element:<div>Projeto do id</div>,
-    //     children:[
-    //         {
-    //             path:'',
-    //             element:<div>Home da tela do projeto!</div>
-    //         },
-    //         {
-    //             path:'equipe',
-    //             element:<div>listagem de mebros da equipe</div>
-    //         },
-    //         {
-    //             path:'calendario',
-    //             element:<div>Listagem de tarefas pendente com data</div>
-    //         },
-    //     ]
-    // }
 ])
